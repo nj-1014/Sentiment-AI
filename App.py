027a3f1ef2027960089cb3fbf2ffaea4ea2a0cc8
Niranjan <niranjanbaiju08@gmail.com>
2026-08-25 20:05:38 +0530

    Update App.py

--- App.py
+++ App.py
@@ -49,6 +49,5 @@ if st.button("Analyze Sentiment"):
         st.write("Text:", text)
         st.write("Sentiment:", sentiment)
         st.write("Compound Score:", scores["compound"])
-        st.write("NEGATIVE")
+        st.write("NEGATIVE                                                     POSITIVE")
         st.progress((scores["compound"] +1)/2)
-        st.write("POSITIVE")
